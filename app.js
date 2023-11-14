8a716fa4b22a07056412de737d3027c5e2d15071
xkvmnt <kevin.amentxd@gmail.com>
2023-11-14 22:15:01 +0100

    added breaklines between each token

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-
 new Vue({
-    el: '#app',
-    data: {
-      userInput: '',
-      responseData: "Output will be updated when the computation is done.. ",
-      ACCESS_TOKEN:'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'
-    },
-    methods: {
-      async fetchData() {
-        const data = { "inputs": this.userInput };
+  el: '#app',
+  data: {
+    userInput: '',
+    responseData: "Output will be updated when the computation is done.. ",
+    ACCESS_TOKEN: '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'
+  },
+  methods: {
+    async fetchData() {
+      const data = { "inputs": this.userInput };
 
-        try {
-          const response = await fetch(
-            "https://api-inference.huggingface.co/models/tomaarsen/span-marker-mbert-base-multinerd",
-            {
-              headers: { Authorization: `Bearer ${this.ACCESS_TOKEN}` },
-              method: "POST",
-              body: JSON.stringify(data),
-            }
-          );
+      try {
+        const response = await fetch(
+          "https://api-inference.huggingface.co/models/tomaarsen/span-marker-mbert-base-multinerd",
+          {
+            headers: { Authorization: `Bearer ${this.ACCESS_TOKEN}` },
+            method: "POST",
+            body: JSON.stringify(data),
+          }
+        );
 
-          const result = await response.json();
-          const entity_group_word=result.map(item => `(entity_group:${item.entity_group} word: ${item.word})`);
-          this.responseData= entity_group_word.join();
-          
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      },
+        const result = await response.json();
+        this.responseData = result;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     },
-  });
+  },
+  computed: {
+    formattedResponseData() {
+      if (Array.isArray(this.responseData)) {
+        return this.responseData.map(item => `(entity_group:${item.entity_group} word: ${item.word})`).join('<br>');
+      } else {
+        return "Output will be updated when the computation is done.. ";
+      }
+    }
+  }
+});
